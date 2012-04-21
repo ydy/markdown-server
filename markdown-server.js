c0bd6f5eaa85ba74736b87520de898a0d3ed3795
Alice <blcuydy+git@gmail.com>
2012-04-22 01:17:07 +0800

    convert markdown to html

--- markdown-server.js
+++ markdown-server.js
@@ -1,20 +1,20 @@
 var http = require('http');
 var fs = require('fs')
+var gh = require('github-flavored-markdown')
 http.createServer(function (req, res) {
   fs.readFile('./' + req.url + '.md', 'utf8', function(err, text){
     if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
       res.write('<b>i</b> couldn\'t find <i>the</i> file');
       res.end();
       return console.log(err)
     }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(text);
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write(gh.parse(text))
     res.end();
   })
 }).listen(1300);
 console.log('Server running at http://127.0.0.1:1300/');
-
 
 
 
