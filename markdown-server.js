e9c0a35877c90d5bd6834f4774bbee1ffbb30a4c
Alice <blcuydy+git@gmail.com>
2012-04-21 18:11:07 +0800

    reads markdown files

--- markdown-server.js
+++ markdown-server.js
@@ -1,11 +1,14 @@
 var http = require('http');
 var fs = require('fs')
 http.createServer(function (req, res) {
-  fs.readFile('text.md', 'utf8', function(err, text){
-    if (err) return console.log(err)
+  fs.readFile('./' + req.url + '.md', 'utf8', function(err, text){
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write('<b>i</b> couldn\'t find <i>the</i> file');
+      res.end();
+      return console.log(err)
+    }
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello Perrin\nYou Requested:\n' + req.url+ '\n');
-    res.write('It has ' + req.url.length + ' letters in it.\n');
     res.write(text);
     res.end();
   })
