7310a1b709620d4ba4cd28f162330f4a2c62c0ef
Alice <blcuydy+git@gmail.com>
2012-04-23 00:14:09 +0800

    makes the markdown server an executable command

---
+++ cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+var mdServer = require('./markdown-server')
+
+mdServer(process.cwd())
--- markdown-server.js
+++ markdown-server.js
@@ -1,21 +1,23 @@
 var http = require('http');
 var fs = require('fs')
 var gh = require('github-flavored-markdown')
-http.createServer(function (req, res) {
-  fs.readFile('./' + req.url + '.md', 'utf8', function(err, text){
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
-      res.write('<b>i</b> couldn\'t find <i>the</i> file');
+module.exports = function initializeMDServer(dir) {
+  http.createServer(function (req, res) {
+    fs.readFile(dir + req.url + '.md', 'utf8', function(err, text){
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+        res.write('<b>i</b> couldn\'t find <i>the</i> file');
+        res.end();
+        return console.log(err)
+      }
+      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+      res.write(gh.parse(text))
       res.end();
-      return console.log(err)
-    }
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.write(gh.parse(text))
-    res.end();
-  })
-}).listen(1300);
-console.log('Server running at http://127.0.0.1:1300/');
-
+    })
+  }).listen(1300);
+  console.log('Server running at http://127.0.0.1:1300/');
+  console.log("Looking for markdown files in the directory: " + dir)
+}
 
 
 
